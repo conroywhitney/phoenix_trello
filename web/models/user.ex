7a6dab34bdd8a14d8473000f4753fc4639e610cf
Conroy Whitney <conroy.whitney@gmail.com>
2016-10-01 01:34:10 -0400

    Fix User registration changeset password
    I guess the password is getting filterd out of the changeset (maybe
    since it's not a part of the model?) so we have to explicitly look to
    see if it's in the params.

--- web/models/user.ex
+++ web/models/user.ex
@@ -11,8 +11,8 @@ defmodule PhoenixTrello.User do
     timestamps()
   end
 
-  @required_fields ~w(first_name last_name email)
-  @optional_fields ~w(encrypted_password)
+  @required_fields ~w(first_name last_name email)a
+  @optional_fields ~w(encrypted_password)a
 
   @derive { Poison.Encoder, only: [:id, :first_name, :last_name, :email] }
 
@@ -24,13 +24,18 @@ defmodule PhoenixTrello.User do
     |> validate_length(:password, min: 5)
     |> validate_confirmation(:password, message: "Password does not match")
     |> unique_constraint(:email, message: "Email already taken")
-    |> generate_encrypted_password
+    |> generate_encrypted_password(params)
   end
 
-  defp generate_encrypted_password(changeset) do
-    case changeset do
-      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
-        put_change(changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+  defp generate_encrypted_password(changeset, params) do
+    case Dict.fetch(params, "password") do
+      {:ok, password} ->
+        case changeset do
+          %Ecto.Changeset{valid?: true} ->
+            put_change(changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+          _ ->
+            changeset
+        end
       _ ->
         changeset
     end
