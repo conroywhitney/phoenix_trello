ff52dbe76247cb397995899ff62dab1134be6058
Conroy Whitney <conroy.whitney@gmail.com>
2016-10-01 01:09:21 -0400

    Add registration reducer

--- web/static/js/reducers/index.js
+++ web/static/js/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routeReducer } from 'redux-simple-router'
 import session from './session'
+import registration from './registration'
 
 export default combineReducers({
+  registration,
   routing: routeReducer,
   session,
 })
